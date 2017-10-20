cbe9f0b85c2bf444a6d8b0c36eeada21dc6b9533
draygera <draygera@gmail.com>
2017-10-20 05:56:54 -0500

    Vulkan and GLFW init stuff

--- sg-game/game.cpp
+++ sg-game/game.cpp
@@ -1,5 +1,60 @@
 /**
  * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
+ * Date              : 20.10.2017
+ * Last Modified Date: 20.10.2017
+ */
+/**
+ * File              : ../sg-game/game.cpp
  * Date              : 19.10.2017
  * Last Modified Date: 19.10.2017
  */
@@ -102,18 +157,33 @@
  */
 
 #include "storm3d.h"
-#include <SDL2/SDL.h>
+/* #include <GL/glew.h> */
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+/* #include <SDL2/SDL.h> */
+using namespace std;
 
 int main(int argc, char *argv[]){
         Storm3D *storm3d;
-        SDL_Window *window;
-        if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0){
-                SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-                return 1;
-        }
 
-        storm3d->test(window);
+        if (!glfwInit()){
+                fprintf( stderr, "Failed to open GLFW Window. If you use an Intel GPU, use version 2.1 of GLFW instead.\n" );
+                glfwTerminate();
+                return -1;
+        }
         
-        SDL_Quit();
+        GLFWwindow *window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+        glfwMakeContextCurrent(window);
+
+        /* SDL_Window *window; */
+        /* if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0){ */
+        /*         SDL_Log("Unable to initialize SDL: %s", SDL_GetError()); */
+        /*         return 1; */
+        /* } */
+
+        /* storm3d->test(window); */
+        
+        /* SDL_Quit(); */
         return 0;
 }
